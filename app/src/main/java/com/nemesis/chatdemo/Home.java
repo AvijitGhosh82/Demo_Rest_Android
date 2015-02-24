f3681dfeded955ceec481e43ea6e6015f567b089
AvijitGhosh82 <avijitg22@gmail.com>
2015-02-23 20:43:29 -0800

    First commit

---
+++ app/src/main/java/com/nemesis/chatdemo/Home.java
@@ -0,0 +1,107 @@
+package com.nemesis.chatdemo;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.List;
+import java.util.Vector;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.astuetz.PagerSlidingTabStrip;
+
+
+public class Home extends ActionBarActivity {
+
+    private Toolbar toolbar;
+    private PagerAdapter mPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
+        SpannableString s = new SpannableString("WeaveIt");
+        if(toolbar != null)
+        {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setTitle(s);
+        }
+
+        this.initialisePaging();
+    }
+
+    private void initialisePaging() {
+
+        List<Fragment> fragments = new Vector<Fragment>();
+        fragments.add(Fragment.instantiate(this, Fragment1.class.getName()));
+        fragments.add(Fragment.instantiate(this, Fragment2.class.getName()));
+        fragments.add(Fragment.instantiate(this, Fragment3.class.getName()));
+        this.mPagerAdapter  = new PagerAdapter(super.getSupportFragmentManager(), fragments);
+        //
+        ViewPager pager = (ViewPager)super.findViewById(R.id.viewpager);
+        pager.setAdapter(this.mPagerAdapter);
+
+        PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        tabs.setViewPager(pager);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class Fragment1 extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.frag1, container, false);
+        }
+    }
+
+    public static class Fragment2 extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.frag2, container, false);
+        }
+    }
+
+    public static class Fragment3 extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.frag3, container, false);
+        }
+    }
+}
---
+++ app/src/main/java/com/nemesis/chatdemo/PagerAdapter.java
@@ -0,0 +1,49 @@
+package com.nemesis.chatdemo;
+
+/**
+ * Created by Avijit Ghosh on 19-02-2015.
+ */
+import java.util.List;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * The <code>PagerAdapter</code> serves the fragments when paging.
+ * @author mwho
+ */
+public class PagerAdapter extends FragmentPagerAdapter {
+    private final String[] TITLES = {"Categories", "Home", "Top Paid"};
+
+    private List<Fragment> fragments;
+    /**
+     * @param fm
+     * @param fragments
+     */
+    public PagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+    /* (non-Javadoc)
+     * @see android.support.v4.app.FragmentPagerAdapter#getItem(int)
+     */
+    @Override
+    public Fragment getItem(int position) {
+        return this.fragments.get(position);
+    }
+
+    /* (non-Javadoc)
+     * @see android.support.v4.view.PagerAdapter#getCount()
+     */
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return TITLES[position];
+    }
+
+    @Override
+    public int getCount() {
+        //return this.fragments.size();
+        return TITLES.length;
+    }
+}
